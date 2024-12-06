49a75a15b05200ab335ca2db283fc64a67825f0e
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-12-06 08:23:59 +0530

    Theme  & font setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 function App() {
+
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <div>App</div>
+    <>
+      <AppShell
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          
+        </AppShell.Navbar>
+        <AppShell.Main>Bot AI</AppShell.Main>
+      </AppShell>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@mantine/core/styles.css';
 import App from './App.tsx'
-
+import { createTheme, MantineProvider } from '@mantine/core'
+const theme = createTheme({
+  fontFamily: 'ubuntu,open sans, sans-serif',
+});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <MantineProvider theme={theme}>
     <App />
+    </MantineProvider>
   </StrictMode>,
 )
