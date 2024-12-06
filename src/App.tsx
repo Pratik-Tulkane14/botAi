ba412ce79cafd95bcba9527c6f1417bb76c6862b
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-12-06 17:46:01 +0530

    feature: created chat page & added static response

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { AppShell, Burger,  Flex, Group } from '@mantine/core';
+import { AppShell, Burger, Button, Flex, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
 
 function App() {
 
   const [opened, { toggle }] = useDisclosure();
+
+  const [stringValue, setStringValue] = useState('');
   return (
     <>
       <AppShell
@@ -29,7 +32,53 @@ function App() {
             Past Conversations
           </div>
         </AppShell.Navbar>
-        <AppShell.Main className='text-[#9785BA]'>Bot AI</AppShell.Main>
+        <AppShell.Main className='text-[#9785BA] font-bold bg-gray-100'>Bot AI
+          {/* Bot Header */}
+          <div className="flex flex-col items-center mt-auto">
+            <h2 className='text-black'>How Can I Help You Today?</h2>
+            <img src={`brandLogo.svg`} className='rounded-full' alt="brand Logo" />
+          </div>
+          {/* Grid Content */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-black mt-10">
+            <div className="bg-white p-6 rounded shodow">
+              <h5>Hi, what is the weather</h5>
+              <p className='text-gray-400 text-sm font-normal'>Get immediate AI generated response</p>
+            </div>
+            <div className="bg-white p-6 rounded shodow">
+              <h5>Hi, what is my location?</h5>
+              <p className='text-gray-400 text-sm font-normal'>Get immediate AI generated response</p>
+            </div>
+            <div className="bg-white p-6 rounded shodow">
+              <h5>Hi, how are you</h5>
+              <p className='text-gray-400 text-sm font-normal'>Get immediate AI generated response</p>
+            </div>
+            <div className="bg-white p-6 rounded shodow">
+              <h5>Hi, what is the temperature</h5>
+              <p className='text-gray-400 text-sm font-normal'>Get immediate AI generated response</p>
+            </div>
+          </div>
+          {/* Chat section */}
+          
+          {/* Input section */}
+          <div className="grid grid-cols-3 md:grid-cols-7 gap-5 mt-10 items-center">
+            <div className="col-span-2 md:col-span-6">
+
+              <input
+                type="text"
+                value={stringValue}
+                onChange={(event) =>
+                  setStringValue(event.currentTarget.value)
+                }
+                className='w-full h-10 px-4 border rounded-md outline-gray-400'
+              />
+            </div>
+            {/* Buttons */}
+            <div className="col-span-1 flex gap-5">
+              <Button variant="filled" color="gray">Ask</Button>
+              <Button variant="filled" color="gray">Save</Button>
+            </div>
+          </div>
+        </AppShell.Main>
       </AppShell>
     </>
   )
